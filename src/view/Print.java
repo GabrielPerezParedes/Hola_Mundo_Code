7221999afdf0b73620bd2194c8974c4c4a5572f2
Gabriel Perez Paredes <gabogabygpp16@gmail.com>
2019-10-28 21:39:55 -0400

    first commit

---
+++ src/view/Print.java
@@ -0,0 +1,22 @@
+package view;
+
+public class Print {
+
+	public static void main(String[] args) {
+		
+		//El codigo utilizado para imprimir algo en pantalla es System.out.print o println("Texto");
+		
+		System.out.println("Hola mundo");
+		
+		/*
+		 * 
+		 * este es un comentario de multiples lineas 
+		 * 
+		 */
+		System.out.println("Este es el primer mensaje en pantalla");
+		
+		
+		
+	}
+
+}
